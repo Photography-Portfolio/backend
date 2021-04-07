321ab15e12d5ef1b2496725804dcd1cdfcfa65be
Mitchell Mullen <michalmullen@gmail.com>
2021-04-07 15:41:59 +0200

    tests have been converted to restTemplate

--- src/test/java/com/buinak/imageapi/controller/ImageControllerTest.java
+++ src/test/java/com/buinak/imageapi/controller/ImageControllerTest.java
@@ -3,15 +3,20 @@ package com.buinak.imageapi.controller;
 import com.buinak.imageapi.entity.Image;
 import com.buinak.imageapi.repository.ImageRepository;
 import com.buinak.imageapi.service.ImageService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
 import java.util.List;
@@ -20,9 +25,16 @@ import java.util.NoSuchElementException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ImageControllerTest {
+
+    @LocalServerPort
+    public int port;
+
+    String BASE_URL;
+
+    RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
 
     @Autowired
     ImageController imageController;
@@ -33,14 +45,18 @@ public class ImageControllerTest {
     @Autowired
     ImageService imageService;
 
+    @Before
+    public void setUp() {
+         BASE_URL = "http://localhost:" + port;
+    }
 
     @Test
     public void addImage() {
         MockMultipartFile multipartFile = new MockMultipartFile("data", "testimg.jpeg", "text/plain", "some xml".getBytes());
 
-        imageController.addImage("NAME1", "DESC1", multipartFile);
+        Image newImage = imageController.addImage("NAME1", "DESC1", multipartFile).getBody();
+        Image image = restTemplate.getForObject(BASE_URL + "/images/" + newImage.getId(), Image.class);
 
-        Image image= imageService.findImageByName("NAME1");
         assertThat(image.getName()).isEqualTo("NAME1");
         assertThat(image.getDescription()).isEqualTo("DESC1");
     }
@@ -48,18 +64,13 @@ public class ImageControllerTest {
     @Test
     @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
     public void listImages() {
-        //setup
-        final String BASE_URL =
-                ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
         MockMultipartFile firstFile = new MockMultipartFile("data", "testimg.jpeg", "text/plain", "some xml".getBytes());
         MockMultipartFile secondFile = new MockMultipartFile("data2", "testimg2.jpeg", "text/plain", "some xml".getBytes());
 
-        //action
         imageController.addImage("list", "list", firstFile);
         imageController.addImage("list2", "list2", secondFile);
+        List<String> images = restTemplate.getForObject(BASE_URL + "/images/links", List.class);
 
-        //assert
-        List<String> images = imageController.getImageLinks().getBody();
         assertThat(images.size()).isEqualTo(2);
         assertThat(images.get(0)).isEqualTo(BASE_URL + File.separator + "testimg.jpeg");
         assertThat(images.get(1)).isEqualTo(BASE_URL + File.separator + "testimg2.jpeg");
@@ -69,27 +80,24 @@ public class ImageControllerTest {
     @Test
     public void patchImage() {
         MockMultipartFile multipartFile = new MockMultipartFile("data", "testimg.jpeg", "text/plain", "some xml".getBytes());
-        ResponseEntity<Image> responseImage = imageController.addImage("NAME2", "DESC2", multipartFile);
-        Image image = responseImage.getBody();
+        Image image = imageController.addImage("NAME2", "DESC2", multipartFile).getBody();
         image.setDescription("string");
         image.setName("string");
 
-        imageController.patchImage(image.getId(), image);
+        Image patchedImage = restTemplate.patchForObject(BASE_URL + "/images/" + image.getId(), image, Image.class);
 
-        Image image2 = imageService.findImageByName("string");
-        assertThat(image2.getName()).isEqualTo("string");
-        assertThat(image2.getDescription()).isEqualTo("string");
+        assertThat(patchedImage.getName()).isEqualTo("string");
+        assertThat(patchedImage.getDescription()).isEqualTo("string");
     }
 
     @Test(expected = NoSuchElementException.class)
     public void deleteImage() {
         MockMultipartFile multipartFile = new MockMultipartFile("data", "testimg.jpeg", "text/plain", "some xml".getBytes());
-        ResponseEntity<Image> responseImage = imageController.addImage("NAME3", "DESC3", multipartFile);
-        Image image = responseImage.getBody();
-        long id = image.getId();
-
+        Image image = imageController.addImage("NAME3", "DESC3", multipartFile).getBody();
         assertThat(imageService.findImageByName("NAME3")).isNotNull();
-        imageController.deleteImageById(id);
-        assertThat(imageController.findImageById(id).getStatusCodeValue()).isEqualTo(404);
+
+        restTemplate.delete(BASE_URL + "/images/" + image.getId());
+
+        assertThat(imageController.deleteImageById(image.getId()).getStatusCodeValue()).isEqualTo(404);
     }
 }
